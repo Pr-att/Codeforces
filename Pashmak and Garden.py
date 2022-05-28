1dc9a76bede056fd0ab19c5d6255e3c7bea1054f
pratt <pp56355@gmail.com>
2022-05-29 00:11:13 +0530

    Initial

--- Pashmak and Garden.py
+++ Pashmak and Garden.py
@@ -1,13 +1,28 @@
 x1, y1, x2, y2 = map(int, input().split())
-distance1 = x2 - x1
-distance2 = y2 - y1
-p = [x1, x2, x2, y2]
-if len(set(p)) > 2:
+x3, y3, x4, y4, count = 0, 0, 0, 0, 0
+distanceX = abs(x2 - x1)
+distanceY = abs(y2 - y1)
+if distanceX == 0:
+    k = distanceY
+    x3 = x1 + k
+    y3 = min(y1, y2)
+    x4 = x1 + k
+    y4 = max(y1, y2)
+elif distanceY == 0:
+    k = distanceX
+    x3 = min(x1, x2)
+    y3 = y1 + k
+    x4 = max(x1, x2)
+    y4 = y1 + k
+else:
+    if distanceX != distanceY:
+        count = 1
+    else:
+        x3 = x1
+        y3 = y2
+        x4 = x2
+        y4 = y1
+if count == 1:
     print(-1)
 else:
-    if distance1 > 0 and distance2 > 0:
-        print(x1, y2, x2, y1)
-    elif distance1 > 0 and distance2 == 0:
-        print(distance1, distance1, x1, distance1)
-    elif distance2 > 0 and distance1 == 0:
-        print(distance2, distance2, distance2, y1)
+    print(x3, y3, x4, y4)
