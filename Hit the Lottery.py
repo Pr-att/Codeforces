54ffad42880f30429782005752c27aad6119625f
Pr-att <pp56355@gmail.com>
2023-02-16 03:21:27 +0530

    Update

--- Hit the Lottery.py
+++ Hit the Lottery.py
@@ -1,3 +1,5 @@
+# TODO: Greedy
+
 amount = int(input())
 deno = [1, 5, 10, 20, 100]
 j = len(deno) - 1
@@ -12,3 +14,22 @@ while amount != 0:
     else:
         j -= 1
 print(count)
+
+# TODO: DP
+
+n = int(input())
+
+
+def func(amount, dp, i, denoms):
+    if amount == 0:
+        return dp[0]
+    elif amount > 0:
+        temp = amount // denoms[i]
+        dp[i] = temp + func(amount - (temp * denoms[i]), dp, i - 1, denoms)
+        return dp[i]
+
+
+array = [0] * 5
+idx = 4
+denomination = [1, 5, 10, 20, 100]
+print(func(n, array, idx, denomination))
